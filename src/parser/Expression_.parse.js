248ef291b6590f82d660c2ffa52403f3ebf3419d
abdallahimam <aemam816@gmail.com>
2018-04-26 20:03:10 +0200

    add files Multi(Statement, Expression), finish the all parse methods

--- src/parser/Expression_.parse.js
+++ src/parser/Expression_.parse.js
@@ -1,4 +1,6 @@
 const Parser = require("../Parser");
+const Expression = require("./Expression_.parse");
+const MultiExpression = require("./MultiExpression");
 const Term = require("./Term.parse");
 
 class Expression_ extends Parser {
@@ -8,11 +10,40 @@ class Expression_ extends Parser {
   }
   parse() {
     if (symbols().indexOf(this.tokens[0].token) != -1) {
+      if (this.validateTokens([this.tokens[0].type])) { //( "&&" | "||" | "==" | "!=" | ">" | "<" | "<=" | ">="| "+" | "-" | "*" | "/" ) Expression Expression_
+        this.shift();
+        new Expression(this.level, this.tokens, this.node);
+        new Expression_(this.level, this.tokens, this.node);
+        return true;
+      }
+      /*
       new Term([0,this.tokens, this.node]);
       new Expression_([0,this.tokens, this.node]);
-      return true;
-    } else {
-      return true;
+      return true;*/
+    } else if (this.validateTokens(["LEFT_SQUARE_B"])) { //  "[" Expression "]" Expression_
+      this.shift();
+      new Expression(this.level, this.tokens, this.node);
+      if (this.validateTokens(["RIGHT_SQUARE_B"])) {
+        this.shift();
+        new Expression_(this.level, this.tokens, this.node);
+        return true;
+      }
+    } else if (this.validateTokens(["DOT", "LENGTH"])) { // "." "length" Expression_
+      this.shift();
+      this.shift();
+      new Expression_(this.level, this.tokens, this.node);
+    } else if (this.validateTokens(["DOT", "IDENTIFIER", "LEFT_ROUND_B"])) { // "." Identifier "(" (Expression ( "," Expression)*)? ")" Expression_
+      this.shift();
+      this.shift();
+      this.shift();
+      new Expression(this.level, this.tokens, this.node);
+      new MultiExpression(this.level, this.tokens, this.node);
+      if (this.validateTokens(["RIGHT_ROUND_B"])) {
+        // this means that we reach to the closing bracket for "(" ("," Expression)* ")"
+        this.shift();
+        new Expression_(this.level, this.tokens, this.node);
+        return true;
+      }
     }
   }
   print() {
