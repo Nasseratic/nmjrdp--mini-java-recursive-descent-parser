bbec62f98b323dfc7e99a5c739c935365deafff8
abdallahimam <aemam816@gmail.com>
2018-04-28 19:19:37 +0200

    update .gotignore file

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ const MainClass = require("./parser/MainClass.parse")
 // Tree Modle
 const newNode = require('./TreeModel');
 
-LA("TESTCASES/1.text").then( tokens => {
+LA("TESTCASES/TESTCASE1_STMT_1.txt").then( tokens => {
   tokens = tokens.filter(i => i.type !== "EOL");
   let root = newNode({id:"ROOT"});
+  console.log(tokens);
   new MainClass(0,tokens,root).print();  
 });
